8457a3156a5348f093e159be78f4d1b2d9ef38a8
Mariya Senina <senina.m.m@gmail.com>
2022-04-02 20:36:26 +0300

    module5 done

--- src/module5/task-2.py
+++ src/module5/task-2.py
@@ -7,11 +7,13 @@ n = len(X)
 
 #удалили столбец Y вставили столбец с единицами
 X = X.drop(columns=['Y'])
-X.insert(loc=1, column='ones', value=([1]*n))
+# X.insert(loc=1, column='ones', value=([1]*n))
+p = X.shape[1] - 1
 
 # выделили строки с тестовыми данными
 test_candy_name1 = "One dime"
 test_candy_name2 = "Nestle Butterfinger"
+
 test_candy1 = X.loc[X['competitorname'] == test_candy_name1]
 test_candy2 = X.loc[X['competitorname'] == test_candy_name2]
 #стерли названия конфет и значения их рейтинга
@@ -24,20 +26,24 @@ n = n - 2
 
 #стерли названия конфет
 x = x.drop(columns=['competitorname'])
+p -= 1
+
 # np.array сразу без заголовков
 # выделили столбец с y
-y = np.array(x[['winpercent']]).reshape(1, n)
-print(y)
-print(np.shape(y))
+y = np.asarray(x[['winpercent']]).reshape(n, 1)
 
 #выделили строки под X
 x = x.drop(columns=['winpercent'])
 
 
-# x-> [68x12] y -> [1x68]
-reg = LinearRegression().fit(x, y)
-reg.fit(candy1, y)
-reg.fit(candy2, y)
+reg = LinearRegression()
+# x-> [68x12] y -> [68x1]
+reg.fit(x, y)
+print(f"{test_candy_name1} prediction:", reg.predict(candy1))
+print(f"{test_candy_name2} prediction:", reg.predict(candy2))
+test_vector = np.asarray([0, 1, 1, 1, 1, 1, 1, 0, 1, 0.26, 0.296]).reshape(1, p)
+# print(test_vector)
+print(f"test vector prediction:", reg.predict(test_vector))
 
 
 
